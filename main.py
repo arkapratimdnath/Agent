be967c8aa273d21fe9dc3aa8517066f3fcca5de3
arkapratimdnath <arkapratimdebnath7@gmail.com>
2025-06-08 19:52:37 +0530

    fixing api keys and using the flash model

--- main.py
+++ main.py
@@ -3,10 +3,33 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
+
+##prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant.Please respond to to the user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+st.title("LangChain Google GenAI")
+input_text = st.text_input("Enter your Question")
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-001")
-response=llm.invoke("I want you to create an weather app that shows the current weather in my city. The app should be simple and user-friendly. Use HTML, CSS, javascript to make 3 seperate files and you should be able  store it in my system. If you are unable to create the files and store them by your own then say what are the tools that are required to make the files and store them lets say you might require Linux based commands to make directory and files how about that.")
-print(response.content)
+outpu_parser= StrOutputParser()
+chain= prompt | llm | outpu_parser
+
+if input_text:
+    response = chain.invoke({"question": input_text})
+    st.write(response)
+
+
+
+# response=llm.invoke("hello")
+# print(response.content)
